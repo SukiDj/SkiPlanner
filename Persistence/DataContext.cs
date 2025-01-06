f1dc76c86b0c7d06fdc48f58c09598248cdea8ea
Aleksandar Djordjevic <djordjevic.1.aleksandar02@gmail.com>
2025-01-07 00:10:46 +0100

    Sitne izmene

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,3 +1,4 @@
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
@@ -7,6 +8,10 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-        
+        // Nije relaciona baza tako da se ne radi tako
+        public DbSet<Skijaliste> Skijalista { get; set; }
+        public DbSet<Staza> Staze { get; set; }
+        public DbSet<Hotel> Hoteli { get; set; }
+        public DbSet<Restoran> Restorani { get; set; }
     }
 }
