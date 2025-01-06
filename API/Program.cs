43c95e5fcbbfb574b75d315b6cbc516d4bfb65bb
Aleksandar Djordjevic <djordjevic.1.aleksandar02@gmail.com>
2025-01-06 13:39:29 +0100

    DataContext

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,16 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    //opt.
+});
 
 var app = builder.Build();
 
